173ccd15a970d087b9608e34590e37286c09b059
Shion Ichikawa <shion1305@gmail.com>
2022-09-03 10:11:22 +0900

    🎉 Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  font-family: 'Open Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+body {
+  margin: 0;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+// import TextBouncing from "@/components/TextBouncing";
+</script>
+<template>
+  <div class="home">
+<!--    <TextBouncing text="Coming Soon..." color="#ffffff" class="text-bouncing"/>-->
+    <div>令和4</div>
+  </div>
+</template>
+<style>
+body{
+  margin: 0;
+}
+</style>
+<style scoped>
+.home {
+  animation: gradient 15s ease infinite;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.text-bouncing {
+  margin: auto;
+  vertical-align: middle;
+}
+
+</style>
